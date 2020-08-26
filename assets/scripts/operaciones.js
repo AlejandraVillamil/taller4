3f680232753503ec9258ac3c6f08c4c968ad1d1b
Alejandra V <eraseunavezpasteleria@gmail.com>
2020-08-26 18:29:04 -0500

    Funciones con css

--- assets/scripts/operaciones.js
+++ assets/scripts/operaciones.js
@@ -3,36 +3,50 @@ const  n1=parseInt(document.getElementById('n1').value)
 //Traer numero 1
 const  n2=parseInt(document.getElementById('n2').value)
 
-let result
-function operaciones(operador){
-    switch(operador){
-        case"suma":
-        n1+n2;
-        alert(n1+n2)
-        console.log(n1+n2)
-        break;
+if(n1!==null && n1!==null)
+{
 
-        case"resta":
-        result= n1-n2;
-        alert(result)
-        console.log(result)
-        break;
-
-        case"multiplicacion":
-        result= n1*n2;
-        alert(result)
-        console.log(result)
-        break;
-
-        case"division":
-        result= n1/n2;
-        alert(result)
-        console.log(result)
-        break;
-        
-        default:
-        alert('No es posible realizar la operación intenta de nuevo')
-        console.log('error')
-        break;
+    let result
+    function operaciones(operador){
+        switch(operador){
+            case"suma":
+            result= n1+n2;
+            print()
+            break;
+    
+            case"resta":
+            result= n1-n2;
+            print()
+            break;
+    
+            case"multiplicacion":
+            result= n1*n2;
+            print()
+            break;
+    
+            case"division":
+            result= n1/n2;
+            print()
+            break;
+            
+            default:
+            alert('No es posible realizar la operación intenta de nuevo')
+            console.log('error')
+            break;
+        }
+        function print(){
+            result_onscreen=document.getElementById('resultado')
+            result_onscreen.innerText = result
+            alert(result)
+            console.log(result)  
+        }
+    
+    
     }
+    
+}else{
+    alert('No es posible realizar la operación intenta de nuevo, ingresando ambos números')
+    console.log('error')
 }
+
+
