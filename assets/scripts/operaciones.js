fdabff341b97dd7511993ce5523d0f483b71b77b
Alejandra V <eraseunavezpasteleria@gmail.com>
2020-08-27 07:00:35 -0500

    Funciones con validación de errores

--- assets/scripts/operaciones.js
+++ assets/scripts/operaciones.js
@@ -1,14 +1,22 @@
-//Traer numero 1
-const  n1=parseInt(document.getElementById('n1').value)
-//Traer numero 1
-const  n2=parseInt(document.getElementById('n2').value)
 
-if(n1!==null && n1!==null)
-{
 
+function operaciones(operador){
     let result
-    function operaciones(operador){
+    //Traer numero 1
+    let  n1=parseInt(document.getElementById('n1').value)
+    console.log(n1)
+    //Traer numero 1
+    let  n2=parseInt(document.getElementById('n2').value)
+    console.log(n2)
+    if(isNaN(n1) ||isNaN(n2) ){
+        console.log('detecta empty')   
+        alert('No es posible realizar la operación intenta de nuevo, ingresando ambos números')
+        console.log('error')
+
+    }else{
+ 
         switch(operador){
+
             case"suma":
             result= n1+n2;
             print()
@@ -34,19 +42,17 @@ if(n1!==null && n1!==null)
             console.log('error')
             break;
         }
-        function print(){
-            result_onscreen=document.getElementById('resultado')
-            result_onscreen.innerText = result
-            alert(result)
-            console.log(result)  
-        }
-    
-    
+            function print(){
+
+                result_onscreen=document.getElementById('resultado')
+                result_onscreen.innerText = result
+                document.getElementById('n1').value="";
+                document.getElementById('n2').value="";
+                alert(result)
+                console.log(result)  
+            }
+
+
+
     }
-    
-}else{
-    alert('No es posible realizar la operación intenta de nuevo, ingresando ambos números')
-    console.log('error')
-}
-
-
+    }
